d4d4f4d39d35db27341222c3f941c448903f22cb
Михаил <kirik.miha502@mail.ru>
2023-11-07 11:51:33 +0300

    new style home.html, and PDF file

--- src/main/java/com/example/test32/controllers/InformationSecurityController.java
+++ src/main/java/com/example/test32/controllers/InformationSecurityController.java
@@ -3,15 +3,14 @@ package com.example.test32.controllers;
 import com.example.test32.models.FileEntity;
 import com.example.test32.repository.FileEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,19 +28,23 @@ public class InformationSecurityController {
         return "information-security";
     }
 
-    @GetMapping("/downloadSecurityInfo/{fileId}")
-    public ResponseEntity<byte[]> downloadFile(@PathVariable Long fileId) {
+    @GetMapping("/viewPdf/{fileId}")
+    public void viewPdf(@PathVariable Long fileId, HttpServletResponse response) {
         Optional<FileEntity> fileOptional = fileEntityRepository.findById(fileId);
 
         if (fileOptional.isPresent()) {
             FileEntity file = fileOptional.get();
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-            headers.setContentDispositionFormData("attachment", file.getFilename());
 
-            return new ResponseEntity<>(file.getContent(), headers, HttpStatus.OK);
+            response.setContentType("application/pdf");
+
+            try {
+                OutputStream outputStream = response.getOutputStream();
+                outputStream.write(file.getContent());
+                outputStream.close();
+            } catch (IOException e) {
+            }
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
-
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
